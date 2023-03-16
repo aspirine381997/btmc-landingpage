a5aae057ee2f127161e6219045281fe0305dab6e
chicongzz <aspirine381997@gmail.com>
2023-03-16 15:36:43 +0700

    feat: config tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+      "2xl": "1440px",
+      "3xl": "1920px",
+    },
+
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "24px",
+        sm: "24px",
+        md: "56px",
+        lg: "64px",
+        xl: "72px",
+        "2xl": "80px",
+        "3xl": "240px",
+      },
+    },
+
+    extend: {},
+  },
+  plugins: [],
+};
