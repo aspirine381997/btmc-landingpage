0bd69eb72f454177a00c7e415da382dcc715cb42
chicongzz <aspirine381997@gmail.com>
2023-03-20 16:33:19 +0700

    fix: add ssr

--- pages/_app.js
+++ pages/_app.js
@@ -3,3 +3,11 @@ import '@/styles/globals.css'
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
+App.getInitialProps = async ({ Component, ctx }) => {
+  const pageProps = Component.getInitialProps
+    ? await Component.getInitialProps(ctx)
+    : {}
+
+  return { pageProps }
+}
